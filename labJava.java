a4c3ea33d8a87c5bf94c6c4f46dae4859827d180
neyd <jev.neydel@gmail.com>
2015-10-05 17:13:05 +0300

    FirstLab

---
+++ labJava.java
@@ -0,0 +1,10 @@
+package info.teib.lab.neyd.lab1;
+
+/**
+ * Created by Женя on 05.10.2015.
+ */
+public class labJava {
+    public static void main(String[] args){
+        System.out.println(1+2+3+4+5);
+    }
+}
