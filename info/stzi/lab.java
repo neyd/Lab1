873d16762f9ae183bb8d017b3fe52ce45aa541fc
neyd <jev.neydel@gmail.com>
2015-10-05 17:48:59 +0300

    This my first lab)

---
+++ info/stzi/lab.java
@@ -0,0 +1,10 @@
+package info.teib.lab.neyd.lab1.info.stzi;
+
+/**
+ * Created by Женя on 05.10.2015.
+ */
+public class lab {
+    public static void main(String[] args){
+        System.out.println(3+4+5+6+7+8);
+    }
+}
